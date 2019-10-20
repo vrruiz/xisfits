c525d919238ea96f6efdf572df4e018e93d6de78
Victor R. Ruiz <rvr@redhat.com>
2019-10-21 00:22:00 +0100

    Refactor fits_write_header_* functions

--- src/fitswriter.rs
+++ src/fitswriter.rs
@@ -15,8 +15,7 @@ pub struct FitsHeaderData {
     pub data_bytes: Vec<u8>,
 }
 
-fn fits_write_header_u64(fits: &mut File, header: &String, value: u64, bytes: &mut u64) -> io::Result<()> {
-    let string = format!("{:8} = {:>19} /{:48}", header, value, "");
+fn fits_write_header(fits: &mut File, string: &String, bytes: &mut u64) -> io::Result<()> {
     println!("FITS header: \"{}\"", string);
     let header_bytes = string.as_bytes();
     fits.write(header_bytes)?;
@@ -24,31 +23,24 @@ fn fits_write_header_u64(fits: &mut File, header: &String, value: u64, bytes: &m
     Ok(())
 }
 
-fn fits_write_header_i64(fits: &mut File, header: &String, value: i64, bytes: &mut u64) -> io::Result<()> {
+fn fits_write_header_u64(fits: &mut File, header: &String, value: u64, bytes: &mut u64) {
     let string = format!("{:8} = {:>19} /{:48}", header, value, "");
-    println!("FITS header: \"{}\"", string);
-    let header_bytes = string.as_bytes();
-    fits.write(header_bytes)?;
-    *bytes = *bytes + header_bytes.len() as u64;
-    Ok(())
+    fits_write_header(fits, &string, bytes);
 }
 
-fn fits_write_header_string(fits: &mut File, header: &String, value: &String, bytes: &mut u64) -> io::Result<()> {
+fn fits_write_header_i64(fits: &mut File, header: &String, value: i64, bytes: &mut u64) {
     let string = format!("{:8} = {:>19} /{:48}", header, value, "");
-    println!("FITS header: \"{}\"", string);
-    let header_bytes = string.as_bytes();
-    fits.write(header_bytes)?;
-    *bytes = *bytes + header_bytes.len() as u64;
-    Ok(())
+    fits_write_header(fits, &string, bytes);
 }
 
-fn fits_write_header_no_comment(fits: &mut File, header: &String, bytes: &mut u64) -> io::Result<()> {
+fn fits_write_header_string(fits: &mut File, header: &String, value: &String, bytes: &mut u64) {
+    let string = format!("{:8} = {:>19} /{:48}", header, value, "");
+    fits_write_header(fits, &string, bytes);
+}
+
+fn fits_write_header_no_comment(fits: &mut File, header: &String, bytes: &mut u64) {
     let string = format!("{:<80}", header);
-    println!("FITS header: \"{}\"", string);
-    let header_bytes = string.as_bytes();
-    fits.write(header_bytes)?;
-    *bytes = *bytes + header_bytes.len() as u64;
-    Ok(())
+    fits_write_header(fits, &string, bytes);
 }
 
 pub fn fits_write(filename: &String, fits_hd: &FitsHeaderData) -> io::Result<()> {
@@ -58,20 +50,20 @@ pub fn fits_write(filename: &String, fits_hd: &FitsHeaderData) -> io::Result<()>
 
     // Write HDU
     println!("FITS write > Write headers");
-    fits_write_header_string(&mut fits, &String::from("SIMPLE"), &String::from("T"), &mut bytes)?;
-    fits_write_header_i64(&mut fits, &String::from("BITPIX"), fits_hd.bitpix, &mut bytes)?;
-    fits_write_header_u64(&mut fits, &String::from("NAXIS"), fits_hd.naxis, &mut bytes)?;
+    fits_write_header_string(&mut fits, &String::from("SIMPLE"), &String::from("T"), &mut bytes);
+    fits_write_header_i64(&mut fits, &String::from("BITPIX"), fits_hd.bitpix, &mut bytes);
+    fits_write_header_u64(&mut fits, &String::from("NAXIS"), fits_hd.naxis, &mut bytes);
     for i in 0..fits_hd.naxis_vec.len() {
         let header_name = format!("NAXIS{}", i+1);
-        fits_write_header_u64(&mut fits, &header_name, fits_hd.naxis_vec[i], &mut bytes)?;
+        fits_write_header_u64(&mut fits, &header_name, fits_hd.naxis_vec[i], &mut bytes);
     }
-    fits_write_header_string(&mut fits, &String::from("EXTEND"), &String::from("T"), &mut bytes)?;
-    fits_write_header_string(&mut fits, &String::from("BZERO"), &String::from("0"), &mut bytes)?;
-    fits_write_header_string(&mut fits, &String::from("BSCALE"), &String::from("1"), &mut bytes)?;
-    // fits_write_header_u64(&mut fits, &String::from("BSCALE"), fits_hd.bscale, &mut bytes)?;
-    // fits_write_header_u64(&mut fits, &String::from("DATAMIN"), fits_hd.datamin, &mut bytes)?;
-    // fits_write_header_u64(&mut fits, &String::from("DATAMAX"), fits_hd.datamax, &mut bytes)?;
-    fits_write_header_no_comment(&mut fits, &String::from("END"), &mut bytes)?;
+    fits_write_header_string(&mut fits, &String::from("EXTEND"), &String::from("T"), &mut bytes);
+    fits_write_header_string(&mut fits, &String::from("BZERO"), &String::from("0"), &mut bytes);
+    fits_write_header_string(&mut fits, &String::from("BSCALE"), &String::from("1"), &mut bytes);
+    // fits_write_header_u64(&mut fits, &String::from("BSCALE"), fits_hd.bscale, &mut bytes);
+    // fits_write_header_u64(&mut fits, &String::from("DATAMIN"), fits_hd.datamin, &mut bytes);
+    // fits_write_header_u64(&mut fits, &String::from("DATAMAX"), fits_hd.datamax, &mut bytes);
+    fits_write_header_no_comment(&mut fits, &String::from("END"), &mut bytes);
 
     // Write HDU (fill the rest of the 2880 byte-block)
     let rest = bytes % 2880;
